6abf7ea833e1edbc7f3b7c2109a94f909b085d8a
RusselldeSilva <s10203242@connect.np.edu.sg>
2021-01-18 21:26:32 +0800

    Test

--- COVID Monitoring System/Program.cs
+++ COVID Monitoring System/Program.cs
@@ -7,6 +7,7 @@ namespace COVID_Monitoring_System
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Write("Gsrgawha");
         }
     }
 }
