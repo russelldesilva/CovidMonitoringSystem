9685b35a299480692363b6b41689ff3807450f8d
RusselldeSilva <s10203242@connect.np.edu.sg>
2021-01-18 21:59:40 +0800

    Remove second Console.Write

--- COVID Monitoring System/Program.cs
+++ COVID Monitoring System/Program.cs
@@ -12,7 +12,6 @@ namespace COVID_Monitoring_System
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.Write("Gsrgawha");
         }
     }
 }
