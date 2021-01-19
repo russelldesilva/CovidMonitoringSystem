51c3a18451860d20ab395d5fe84d1759fe5f1398
RusselldeSilva <s10203242@connect.np.edu.sg>
2021-01-19 13:48:42 +0800

    Add attributes to SHNFacility and TravelEntry classes

--- COVID Monitoring System/SHNFacility.cs
+++ COVID Monitoring System/SHNFacility.cs
@@ -12,5 +12,85 @@ namespace COVID_Monitoring_System
 {
     class SHNFacility
     {
+        private string facilityName;
+
+        public string FacilityName
+        {
+            get { return facilityName; }
+            set { facilityName = value; }
+        }
+
+        private int facilityCapacity;
+
+        public int FacilityCapacity
+        {
+            get { return facilityCapacity; }
+            set { facilityCapacity = value; }
+        }
+
+        private int facilityVacancy;
+
+        public int FacilityVacancy
+        {
+            get { return facilityVacancy; }
+            set { facilityVacancy = value; }
+        }
+
+        private double distFromAirCheckpoint;
+
+        public double DistFromAirCheckpoint
+        {
+            get { return distFromAirCheckpoint; }
+            set { distFromAirCheckpoint = value; }
+        }
+
+
+        private double distfromSeaCheckpoint;
+
+        public double DistFromSeaCheckpoint
+        {
+            get { return distfromSeaCheckpoint; }
+            set { distfromSeaCheckpoint = value; }
+        }
+
+        private double distFromLandCheckpoint;
+
+        public double DistFromLandCheckpoint
+        {
+            get { return distFromLandCheckpoint; }
+            set { distFromLandCheckpoint = value; }
+        }
+
+        public SHNFacility() { }
+
+        public SHNFacility(string n, int c, int v, double a, double s, double l)
+        {
+            FacilityName = n;
+            FacilityCapacity = c;
+            FacilityVacancy = v;
+            DistFromAirCheckpoint = a;
+            DistFromSeaCheckpoint = s;
+            DistFromLandCheckpoint = l;
+        }
+        
+        //CalculateTravelCost
+
+        public bool IsAvailable()
+        {
+            if (FacilityVacancy < FacilityCapacity)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Facility Name: " + FacilityName + "\tFacility Capacity: " + FacilityCapacity + "\tFacility Vacancy: " + FacilityVacancy + "\tDistance from air checkpoint: " + DistFromAirCheckpoint
+                + "\tDistance from sea checkpoint: " + DistFromSeaCheckpoint + "\tDistance from land checkpoint: " + DistFromLandCheckpoint;
+        }
     }
 }
--- COVID Monitoring System/TravelEntry.cs
+++ COVID Monitoring System/TravelEntry.cs
@@ -12,5 +12,76 @@ namespace COVID_Monitoring_System
 {
     class TravelEntry
     {
+        private string lastCountryOfEmbarkation;
+
+        public string LastCountryOfEmbarkation
+        {
+            get { return lastCountryOfEmbarkation; }
+            set { lastCountryOfEmbarkation = value; }
+        }
+
+        private string entryMode;
+
+        public string EntryMode
+        {
+            get { return entryMode; }
+            set { entryMode = value; }
+        }
+
+        private DateTime entryDate;
+
+        public DateTime EntryDate
+        {
+            get { return entryDate; }
+            set { entryDate = value; }
+        }
+
+        private DateTime shnEndDate;
+
+        public DateTime SHNEndDate
+        {
+            get { return shnEndDate; }
+            set { shnEndDate = value; }
+        }
+
+        private SHNFacility shnStay;
+
+        public SHNFacility SHNStay
+        {
+            get { return shnStay; }
+            set { shnStay = value; }
+        }
+
+        private bool isPaid;
+
+        public bool IsPaid
+        {
+            get { return isPaid; }
+            set { isPaid = value; }
+        }
+
+
+        public TravelEntry() { }
+
+        public TravelEntry(string l, string m, DateTime entry)
+        {
+            LastCountryOfEmbarkation = l;
+            EntryMode = m;
+            EntryDate = entry;
+            SHNEndDate = new DateTime();
+            SHNStay = new SHNFacility();
+            IsPaid = false;
+        }
+
+        public void AssignSHNFacility(SHNFacility s)
+        { 
+            SHNStay = s;
+        }
+        //Calculate SHNDuration
+        public override string ToString()
+        {
+            return "Last Country of Embarkation: " + LastCountryOfEmbarkation + "\tEntry mode: " + EntryMode + "\tEntry Date: " + EntryDate + "" +
+                "\tSHN End Date: " + SHNEndDate + "\tSHN Facility: " + SHNStay + "\tPaid: " + IsPaid;
+        }
     }
 }
