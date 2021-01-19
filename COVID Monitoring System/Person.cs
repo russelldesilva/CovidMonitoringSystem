c3f8c40e327d8ae4de18f3ff17bc75d3aa246b90
Kang Ayken Lee <s10206172@connect.np.edu.sg>
2021-01-19 16:54:07 +0800

    Person class

--- COVID Monitoring System/Person.cs
+++ COVID Monitoring System/Person.cs
@@ -10,7 +10,70 @@ using System.Text;
 
 namespace COVID_Monitoring_System
 {
-    class Person
+    abstract class Person
     {
-    }
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private List<SafeEntry> safeEntryList;
+
+		public List<SafeEntry> SafeEntryList
+		{
+			get { return safeEntryList; }
+			set { safeEntryList = value; }
+		}
+
+		private List<TravelEntry> travelEntryList;
+
+		public List<TravelEntry> TravelEntryList
+		{
+			get { return travelEntryList; }
+			set { travelEntryList = value; }
+		}
+
+		public Person() { }
+
+		public Person(string name)
+		{
+			this.name = name;
+		}
+
+		public void AddTravelEntry(TravelEntry travelEntry)
+		{
+			TravelEntryList.Add(travelEntry);
+		}
+
+		public void AddSafeEntry(SafeEntry safeEntry)
+		{
+			SafeEntryList.Add(safeEntry);
+		}
+
+		public abstract double CalculateSHNCharges();
+
+		public override string ToString()
+		{
+			Console.WriteLine("Name: " + Name);
+			foreach (SafeEntry s in SafeEntryList)
+			{
+				Console.WriteLine(s.ToString());
+			}
+			foreach (TravelEntry t in TravelEntryList)
+			{
+				Console.WriteLine(t.ToString());
+			}
+
+			return "";
+		}
+		
+
+		
+
+
+
+	}
 }
